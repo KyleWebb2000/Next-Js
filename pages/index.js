04f4db313934a903137da7e1ca27a6cac22338bf
Kyle Webb <Kylewebb3577@gmail.com>
2023-02-06 22:32:33 +0000

    WIP: initial layout design

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,45 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Header.module.css';
+import sectionStyles from '../styles/Section.module.css';
 import Link from 'next/link';
 
-export default function Home() {
+export default function Header() {
   return (
     <body className={styles.body}>
-      <div className={styles.container}>
-        <Head>
-          <title>Kyle's Blog</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+      <main>
+        <div className={styles.container}>
+          <Head>
+            <title>Kyle's Blog</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-        <main>
-          <div  className={styles.headerContainer}>
+            <div  className={styles.headerContainer}>
 
-            <div  className={styles.headerTitle}>
-              <h1>Welcome To My Next.js Website</h1>
-            </div>
+              <div  className={styles.headerTitle}>
+                <h1>Next.js</h1>
+                <h1>Next.js</h1>
+              </div>
 
-            <div  className={styles.headerNav}>
-              <p className={styles.navLinks}><Link className={styles.textLink} href="/posts/first-post">My Post</Link></p>
-              <p className={styles.navLinks}><Link className={styles.textLink}  href="/about">About Me</Link></p>
-              <p className={styles.navLinks}><Link className={styles.textLink}  href="/contact">Contact</Link></p>
-            </div>
+              <div  className={styles.headerNav}>
+                <p className={styles.navLinks}><Link className={styles.textLink} href="/posts/first-post">My Post</Link></p>
+                <p className={styles.navLinks}><Link className={styles.textLink}  href="/about">About Me</Link></p>
+                <p className={styles.navLinks}><Link className={styles.textLink}  href="/contact">Contact</Link></p>
+              </div>
 
-          </div>
-        </main>
-    
-      </div>
+            </div> 
+
+        </div>
+        
+        <div className={sectionStyles.sectionContainer}>
+          <div className={sectionStyles.section}></div>
+
+          <div className={sectionStyles.section}></div>
+
+          <div className={sectionStyles.section}></div>
+        </div>
+
+      </main>
     </body>
+
   )
 }
